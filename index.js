f2ebcfdad7c12961329d8c4693a79fbd73465978
jaypatel342005 <pateljay97378@gmail.com>
2024-08-10 14:29:48 +0530

    bot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
-const simpleGit = require('simple-git');
+const simpleGit = require('simple-git')();
 
 const FILE_PATH = './data.json';
 
@@ -8,7 +8,11 @@ const makeCommit = async (n) => {
     // Dynamically import the 'random' module
     const { default: random } = await import('random');
 
-    if (n === 0) return simpleGit().push();
+    if (n === 0) {
+        // Pull the latest changes from the remote repository
+        await simpleGit.pull('origin', 'main');
+        return simpleGit.push();
+    }
     
     const x = random.int(0,54);
     const y = random.int(0,6);
@@ -19,8 +23,9 @@ const makeCommit = async (n) => {
     };
     console.log(DATE);
     jsonfile.writeFile(FILE_PATH, data, () => {
-        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n));
+        simpleGit.add([FILE_PATH])
+            .commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n));
     });
 };
 
-makeCommit(100);
+makeCommit(10);
